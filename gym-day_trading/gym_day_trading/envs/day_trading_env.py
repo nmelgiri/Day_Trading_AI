14e756c1383b9ec786e09c74fa34231d7f7d6649
MelNi001 <nmelgiri@edui.uwaterloo.ca>
2020-01-08 13:11:20 -0500

    working on gym

--- gym-day_trading/gym_day_trading/envs/day_trading_env.py
+++ gym-day_trading/gym_day_trading/envs/day_trading_env.py
@@ -1,4 +1,5 @@
 import gym
+from numpy import inf
 from gym import error, spaces, utils
 from gym.utils import seeding
 
@@ -6,8 +7,12 @@ from gym.utils import seeding
 class DayTradingEnv(gym.Env):
 	metadata = {'render.modes': ['human']}
 
-	def __init__(self):
-		...
+	def __init__(self, action_space=spaces.Box(-1, 1, (1,)), history=5):
+		self.action_space = action_space
+		self.history = history
+		self.observation_space = spaces.Tuple(spaces={
+			spaces.Box(low=-1, high=1, shape=(history,))
+		})
 
 	def step(self, action):
 		...
--- testing_gym.py
+++ testing_gym.py
@@ -1,4 +1,25 @@
 import gym
+from gym.spaces import Discrete, Box
 import gym_day_trading
 
-env = gym.make("day_trading-v0")
+import json
+
+SETTING = "Setting 1: Raw Numerical Inputs"
+
+
+def get_action_space_history():
+	settings = json.load(open("setup.json", "r"))
+	inputs, history = settings["inputs"], settings["history"]
+	if SETTING == "Setting 1: Raw Numerical Inputs":
+		high, low = settings[SETTING]["Action Space"]["upper limit"], settings[SETTING]["Action Space"]["lower limit"]
+		action_space = Box(low, high, shape=(history, inputs))
+		return action_space, history
+
+
+def main():
+	history, action_space = get_action_space_history()
+	env = gym.make("day_trading-v0", action_space=action_space, history=history)
+
+
+if __name__ == '__main__':
+	main()
